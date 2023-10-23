5ae8cac4c743b7557e68095ad4b2147b08ab1a9e
ryu <cristellechoo@hotmail.com>
2023-10-23 17:11:33 +0800

    create server.js file and config folder with database.js file

---
+++ server.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+require("./config/database");
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+// const debug = require("debug")("nextfit:server");
+
+
+//* Routers
+// const usersRouter = require("./routes/usersRouter");
+// const wardrobeRouter = require("./routes/wardrobeRouter");
+// const outfitRouter = require("./routes/outfitsRouter");
+
+//* App
+const app = express();
+
+//* Middlewares
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "dist")));
+// Middleware to verify token and assign user object of payload to req.user.
+// app.use(checkToken);
+
+//* Routes -> all routes to start with /api
+// app.use("/api/users", usersRouter);
+// app.use("/api/wardrobe", ensureLoggedIn, wardrobeRouter);
+// app.use("/api/outfits", ensureLoggedIn, outfitRouter);
+
+//? This should be the last route -> this is for react router
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "dist", "index.html"));
+// });
+
+//* Listener
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log(`Express app running on port ${port}`);
+});
