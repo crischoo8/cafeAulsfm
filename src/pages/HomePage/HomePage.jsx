1d933bbc7278a9c242e301b2e41b5a888c865276
ryu <cristellechoo@hotmail.com>
2023-11-02 22:45:20 +0800

    update src for mp4 file rendered on HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,12 @@
 import '../../../public/assets/lsfmbackground.mp4'
 
-export default function HomePage({user}) {
+export default function HomePage() {
 
     return(
         <>
-         {/* <h1>this is the HomePage</h1>
-         <h1>Welcome Home {user.username}! Ⴚტ◕‿◕ტჂ</h1>
-         <br/> */}
          <div>
             <video loop autoPlay muted>
-                <source src={'../../../public/assets/lsfmbackground.mp4'} type='video/mp4'/>
+                <source src={'/assets/lsfmbackground.mp4'} type='video/mp4'/>
             </video>
          </div>
         </>
